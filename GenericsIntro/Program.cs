6134bd39c60babb49776ee46414e858302ac2326
bekci <alaraalacan@yandex.com>
2021-01-21 00:02:48 +0200

    20 Ocak Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericsIntro;
+using System;
+
+
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        MyList<int> myList = new MyList<int>();
+        isimler.Add("Engin");
+    }
+
+    }
+
