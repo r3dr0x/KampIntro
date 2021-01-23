54507a868ce079f8f969374b5a6a5df664e4cb8d
bekci <alaraalacan@yandex.com>
2021-01-23 23:58:52 +0200

    23 Ocak

---
+++ OOP3/TasitKredimanager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class TasitKredimanager : IKrediManager
+    {
+        public void BiseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            //
+            //
+
+            Console.WriteLine("Taşıt Kredisi Ödeme Planı Hesaplandı");
+        }
+    }
+}
